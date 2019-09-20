ce62a2e415b7f8da2b6f91a8ef4b47001280b69c
Dale Pancho <dalepancho7@gmail.com>
2019-09-20 11:03:37 -0700

    MVP?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route } from "react-router-dom";
+import Characters from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
+import Search from "./components/SearchForm";
+// import CharacterList from "./components/CharacterList";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={Characters} />
+      <Route path="/search" component={Search} />
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+// import Button from "react-bootstrap/Button"
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <div>
+      <Card.Img src={props.image}/>
+      <Card.Title>{props.name}</Card.Title>
+      <Card.Body>
+        <Card.Text>
+          <span>Status: {props.status}</span>
+          <span>Gender: {props.gender}</span>
+          <span>Species: {props.species}</span>
+        </Card.Text>
+      </Card.Body>
+      <button>WHAAAAAT?</button>
+    </div>
+  )
 }
+
